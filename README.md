70d8ec001f8b2ff17bf97b53db35d11dbc26dfe7
adyhariri <mahmoudalhariri1994@gmail.com>
2021-04-13 13:52:16 +0300

    la03 done , we add two question to the guessing game

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ABOUT-ME
+
+# hello visiters it is guessing game site
+
+### guessing game 
+
+it is a funny game to view more information about me for visiters
+
+**lab03**
+we add on this lab two more question about me and the visiter should guess the right answer
+  
+  >  ***first question*** will ask about the month that i was born , the visiter has 4 opportionities to guess the correct answer .
+  i used : for loop , if else statment , prompt , alert and console 
+  >  ***seconde question*** will ask about one of my favorite cities , the visiter has 6 opportionities to guess the correct answer .
+  i used same as first question technickes  beside array .
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+let score=0;
 alert('hello and welcome to my website ');
 let userName = prompt('what is your name ? ');
 alert('hello ' + userName + ' let us play guessing game ');
@@ -7,6 +8,7 @@ alert('your answer must be y/n or yes/no');
 let firstQuestion = prompt("Is my age more than 20 ?").toLowerCase();
 if (firstQuestion === 'y' || firstQuestion === 'yes') {
     alert('Your answer  is correct');
+    score++;
 } else if (firstQuestion === 'n' || firstQuestion === 'no') {
     alert('Your answer is wrong');
 } else {
@@ -15,6 +17,7 @@ if (firstQuestion === 'y' || firstQuestion === 'yes') {
 let secondeQuestion = prompt("do i like animals  ?").toLowerCase();
 if (secondeQuestion === 'n' || secondeQuestion === 'no') {
     alert('Your answer  is correct');
+    score++;
 } else if (secondeQuestion === 'y' || secondeQuestion === 'yes') {
     alert('Your answer is wrong');
 } else {
@@ -23,6 +26,7 @@ if (secondeQuestion === 'n' || secondeQuestion === 'no') {
 let thirdQuestion = prompt("Is my favorite color nevy?").toLowerCase();
 if (thirdQuestion === 'y' || thirdQuestion === 'yes') {
     alert('Your answer  is correct');
+    score++;
 } else if (thirdQuestion === 'n' || thirdQuestion === 'no') {
     alert('Your answer is wrong');
 } else {
@@ -31,6 +35,7 @@ if (thirdQuestion === 'y' || thirdQuestion === 'yes') {
 let fourthQuestion = prompt("Do i like programming?").toLowerCase();
 if (fourthQuestion === 'y' || fourthQuestion === 'yes') {
     alert('Your answer  is correct');
+    score++;
 } else if (fourthQuestion === 'n' || fourthQuestion === 'no') {
     alert('Your answer is wrong');
 } else {
@@ -39,9 +44,46 @@ if (fourthQuestion === 'y' || fourthQuestion === 'yes') {
 let fifthQuestion = prompt("realMadrid is the best football team?").toLowerCase();
 if (fifthQuestion === 'y' || fifthQuestion === 'yes') {
     alert('sure it is correct');
+    score++;
 } else if (fifthQuestion === 'n' || fifthQuestion === 'no') {
     alert('Your answer is wrong');
 } else {
     alert('You only can answer using y/n, yes/no. Now you will be taken to the next question');
 }
-alert('game done , thank you ' + userName + ' for your visiting');
+//alert('game done , thank you ' + userName + ' for your visiting');
+for(let i=0;i<=4;i++){
+    if (i==4){
+        alert('sorry you spent all your opportunities , the correct answer is 4');
+        break;
+    }
+    let sixthQuestion = prompt ("guess which month i was porn on ","use number instead of month name like 1,2,3..etc");
+     if(sixthQuestion==4){
+         alert('you are correct');
+         score++;
+         break;
+     }else if (sixthQuestion<4){
+         alert('too low, try again');
+     }else {
+        alert('too high, try again');
+     }
+    }
+let cities = ['damascus','daraa','amman','irbid','saida'];
+console.log(cities);
+for(let i=0 ; i<=6;i++){
+    if (i==6){
+        alert('sorry you spent all your opportunities , here is my favorite arabic cities :'+cities);
+        break;
+    }
+    let siventhQuestion = prompt ("guess one of my favorite arabic cities ").toLowerCase();
+    for(let j=0;j<cities.length;j++){
+        if(siventhQuestion==cities[j]){
+            alert('it is correct answer , here is my favorite arabic cities :'+cities);
+            score++;
+            j=6765;
+            i=767;
+        }
+       
+    }
+}
+console.log(score);
+alert('your score is '+score);
